7b1f5bbe6479cbfac385a710123ecbae3e179e48
KamranMahdavi <Kamranz.mahdaviz@gmail.com>
2026-06-09 10:31:45 -0300

    Update backend logic

--- backend_script.py
+++ backend_script.py
@@ -1,15 +1,54 @@
-from roman_decimal_functions import decimal_to_roman
+from number_conversion_functions import *
 import sys
 
-decimal_input = sys.argv[1]
+input_form = sys.argv[1].upper()
+output_form = sys.argv[2].upper()
+number = sys.argv[3]
+mode = sys.argv[4] if len(sys.argv) > 4 else "SIGNED"
 
 try:
-    print(decimal_to_roman(decimal_input))
+    match input_form, output_form:
+        case "BIN", "DEC":
+            if mode == "SIGNED":
+                print(binary_to_decimal(number)) 
+            else:
+                print(binary_to_decimal(number, signed=False))
+        case "DEC", "BIN":
+            if mode == "SIGNED":
+                print(decimal_to_binary(number))
+            else:
+                print(decimal_to_binary(number, signed=False))
+        case "BIN", "HEX":
+            print(binary_to_hex(number))
+        case "HEX", "BIN":
+            print(hex_to_binary(number))
+        case "BIN", "RMN":
+            print(binary_to_roman(number))
+        case "RMN", "BIN":
+            print(roman_to_binary(number))
+        case "DEC", "HEX": 
+            if mode == "SIGNED":
+                print(decimal_to_hex(number))
+            else:
+                print(decimal_to_hex(number, signed=False))
+        case "HEX", "DEC":
+            if mode == "SIGNED":
+                print(hex_to_decimal(number))
+            else:
+                print(hex_to_decimal(number, signed=False))
+        case "DEC", "RMN":
+            print(decimal_to_roman(number))
+        case "RMN", "DEC":
+            print(roman_to_decimal(number))
+        case "HEX", "RMN":
+            print(hex_to_roman(number))
+        case "RMN", "HEX":
+            print(roman_to_hex(number))
+
     sys.exit(0)
 
 except Exception as e:
-    print("Invalid input. You should give a decimal number.", file=sys.stderr)
-    print(f"Error Message: {repr(e)}", file=sys.stderr)
+    print(f"Error Message: {str(e)}", file=sys.stderr)
     sys.exit(1)
 
     
